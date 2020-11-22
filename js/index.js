e2a40c6a9964be415a174128a0f9717a58e32140
nathanaelvictorious <victoriousnathan55@gmail.com>
2020-11-23 00:29:33 +0700

    node

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/home', (req, res) => {
+    return res.send('Home Screen')
+})
+
+app.listen(port, () => {
+    console.log('Server listening on port $(port)');
+})
+
+console.log("Hello World");

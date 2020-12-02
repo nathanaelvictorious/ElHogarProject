295717634a13946bd17a0d741f43221143160e19
nathanaelvictorious <victoriousnathan55@gmail.com>
2020-12-02 12:04:50 +0700

    2 December

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const path = require('path');
-const propertyRouter = require('./routes/router.js');
+//const propertyRouter = require('./routes/router.js');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
 const Property = require('./models/property.js');
+const propertiesAll = Property.find()
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 app.set('views', (__dirname, './views'));
 
 
-app.get('/home', (req, res) => {
-    res.sendFile(`${__dirname}/public/home.html`)
+app.get('/home', async (req, res) => {
+    res.render('home.ejs')
 })
 
 app.get('/aboutus', (req, res) => {
@@ -29,7 +30,28 @@ app.get('/agents', (req, res) => {
     res.sendFile(`${__dirname}/public/agents-grid.html`)
 })
 
-app.use('/property', propertyRouter);
+app.get('/:id', async (req, res) => {
+    console.log('Hello kontl');
+    const properties = await Property.findById(req.params.id);
+    if (properties== null) res.redirect('/home');
+
+    res.render('property-single.ejs', { properties: properties});
+})
+
+/*app.post('/search', (req, res) => {
+    let propertyQuery = new RegExp("^" + req.body.query)
+    Property.find({name: {$regex: propertyQuery}})
+
+    .then(property => {
+        res.json(property)
+    })
+
+    .catch(err => {
+        console.log(err)
+    })
+}) */
+
+//app.use('/property', propertyRouter);
 
 
 
--- routes/router.js
+++ routes/router.js
@@ -5,9 +5,10 @@ const router = express.Router()
 console.log('router connected');
 
 router.get('/:id', async (req, res) => {
-    const properties = await Property.findById(req.params.id)
-    if (properties== null) res.redirect('/home')
-    res.render('property-single.ejs', { properties: properties})
+    console.log('Hello kontl');
+    const properties = await Property.findById(req.params.id);
+    if (properties== null) res.redirect('/home');
+    res.render('property-single.ejs', { properties: properties});
 })
 
 module.exports = router

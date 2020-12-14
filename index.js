94735044686732ee048fb902c5e6964c9cafb9b8
nathanaelvictorious <victoriousnathan55@gmail.com>
2020-12-15 01:23:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
-// const propertyRouter = require('./routes/router.js');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000
 
 const bodyParser = require("body-parser");
-const Properti = require('./models/properti')
 const propertyRouter = require('./routes/property')
 const methodOverride = require('method-override')
 const multer = require('multer')
@@ -20,6 +18,7 @@ const session = require("express-session");
 const passport = require("passport");
 
 const Property = require('./models/property.js');
+const Properti = require('./models/properti')
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
@@ -29,14 +28,12 @@ app.set('views', (__dirname, './views'));
 require("./config/passport")(passport);
 
 //db config
-
 const db = require("./config/keys").MongoURI;
 
 //connect mongodb
-
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then(() => console.log("mongoDB Terkoneksi..."))
+  .then(() =>)
   .catch((err) => console.log(err));
 
 
@@ -71,20 +68,16 @@ app.get('/search', async (req, res) => {
         const Km = req.query.km
         const Carslot = req.query.carslot
         const Price = req.query.price
-        /*const Kt = parseInt(req.query.kt)
-        const Km = parseInt(req.query.km)
-        const Carslot = parseInt(req.query.carslot)
-        const Price = parseInt(req.query.price)*/
 
         const properties = await Property.find({
             $or: [
-                {$or: [{name: {$regex: Keyword, $options: 'i'}}, {desc: {$regex: Keyword, $options: 'i'}}]},
+                {$or: [{nama: {$regex: Keyword, $options: 'i'}}, {desc: {$regex: Keyword, $options: 'i'}}]},
                 {jenis: {$eq: Jenis},
                  lokasi: {$eq: Lokasi},
                  kt: {$eq: Kt},
                  km: {$eq: Km},
-                 carslot: {$eq: Carslot},
-                 price: {$gt: 0, $lte: Price}}
+                 cs: {$eq: Carslot},
+                 harga: {$gt: 0, $lte: Price}}
             ]
         }) 
         console.log(properties)
@@ -93,22 +86,6 @@ app.get('/search', async (req, res) => {
     console.error(err);
     }
 })
-
-// Get property single buat id
-// ------------------------------------------
-// Note : get yg ini bentrok
-app.get('/properties/:id', async (req, res) => {
-    console.log('Success');
-
- const properties = await Properti.findById(req.params.id)
-    if (properties== null) res.redirect('/home');
-
-     res.render('property-single.ejs', { 
-         properties: properties
-     })
- })
-
-
 
 app.use('/css', express.static(__dirname + 'public/css'))
 
@@ -123,7 +100,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'))
 
 // express session middleware
-
 app.use(
   session({
     secret: "rahasia",

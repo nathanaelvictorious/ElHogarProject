da042dbf4fea804dfaf8c7059e2c67b4b15d98d5
Andrean Setiawan <70674884+andrean535180002@users.noreply.github.com>
2020-12-15 00:12:30 +0700

    push

--- index.js
+++ index.js
@@ -25,6 +25,30 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 app.set('views', (__dirname, './views'));
 
+//passport config
+require("./config/passport")(passport);
+
+//db config
+
+const db = require("./config/keys").MongoURI;
+
+//connect mongodb
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => console.log("mongoDB Terkoneksi..."))
+  .catch((err) => console.log(err));
+
+app.use(expressLayouts);
+
+
+//bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+
+// static files
+app.use(express.static('public'))
+
 
 app.get('/', async (req, res) => {
     const location = await Property.find().distinct('lokasi')
@@ -83,42 +107,19 @@ app.get('/search', async (req, res) => {
 // Get property single buat id
 // ------------------------------------------
 // Note : get yg ini bentrok
-app.get('/property/:id', async (req, res) => {
-    console.log('Success');
+// app.get('/property/:id', async (req, res) => {
+//     console.log('Success');
 
-    const properties = await Property.findById(req.params.id)
-    if (properties== null) res.redirect('/home');
+//     const properties = await Property.findById(req.params.id)
+//     if (properties== null) res.redirect('/home');
 
-    res.render('property-single.ejs', { 
-        properties: properties
-    })
-})
-
-
-//passport config
-require("./config/passport")(passport);
-
-//db config
-
-const db = require("./config/keys").MongoURI;
-
-//connect mongodb
-
-mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then(() => console.log("mongoDB Terkoneksi..."))
-  .catch((err) => console.log(err));
-
-// ejs
-app.use(expressLayouts);
-app.set("view engine", "ejs");
-
-//bodyparser
-app.use(express.urlencoded({ extended: false }));
+//     res.render('property-single.ejs', { 
+//         properties: properties
+//     })
+// })
 
 
-// static files
-app.use(express.static('public'))
+
 app.use('/css', express.static(__dirname + 'public/css'))
 
 // express session middleware
